668c1443a547b5b30a989870a0426729a77c52bc
Luis Guilherme Bezerra Bispo <luguilherm@engdb.infra>
2020-11-19 21:13:56 -0300

    Estruturando

---
+++ my-app/src/services/index.js
@@ -0,0 +1,3 @@
+//export { default as AuthService } from './AuthService';
+//export { default as CidadaoService } from './CidadaoService';
+//export { default as ConsultorService } from './ConsultorService';
